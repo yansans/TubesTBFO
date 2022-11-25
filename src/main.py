812cd715e5124ba2265ddb1b21bbf4391db3b744
yansans <66671259+yansans@users.noreply.github.com>
2022-11-25 16:08:03 +0700

    almost fix

--- src/main.py
+++ src/main.py
@@ -1,18 +1,41 @@
 import sys
 import CYK_Algorithm
 import cnf_convert
+import string
+
+abc = string.ascii_letters + string.digits + string.punctuation + ""
+
+kurung = ['(' , ')']
 
 if __name__ == "__main__":
-    cnf_convert.CFG_to_CNF_convert('grammar.txt', 'cnf.txt')
+    # cnf_convert.CFG_to_CNF_convert('grammar.txt', 'cnf.txt')
     parser = CYK_Algorithm.CYK()
-    parser.insert_grammar('cnf.txt')
+    parser.insert_grammar('test/cnf.txt')
     string = ""
     with open(sys.argv[1], 'r') as source_code:
         for lines in source_code:
             string += lines.replace('\n', ' ')
-    string += '    '
+    # string += '     '
+    string = string.split(' ')
     print(string)
-    status = parser.check_grammar(string)
+    newstring = ""
+    for i in string:
+        if len(i) > 0:
+            if i[0] == '"':
+                for j in i:
+                    newstring += j + "   "
+                continue
+        if i != '':
+            newstring += i + '     '
+    
+    newstring = newstring.replace(kurung[0], kurung[0] + '     ')
+    newstring = newstring.replace(kurung[1], '     ' + kurung[1])
+    newstring = newstring.replace(';', '     ;')
+    # newstring = newstring.replace('"', '     "      ')
+    newstring += "     "
+    # newstring = newstring.replace('  ', ' ')
+    print(newstring)
+    status = parser.check_grammar(newstring)
     if status:
         print("Accepted")
     else:
---
+++ test/test2.js
@@ -0,0 +1,28 @@
+const x ;   
+let N = 10 ;  
+
+while ( x  <=  10 ) {
+    console.log() ;
+    if ( x === 5 ) {
+        console.log() ;
+    }
+}
+
+{
+    const nice = 69 ;
+    const life = 42 ;
+
+    if ( nice === life ) {
+        answer = null ;
+    }
+}
+
+try {
+    console.log() ;
+}
+catch (err) {
+    var error = true;
+}
+finally {
+    var error = null;
+}
