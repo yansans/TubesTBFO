faaa7c90b3d52dbf3e4feb72bed1eb3f2c326aba
rayhanp1402 <rayhan.hanif14maulana@gmail.com>
2022-11-25 17:07:50 +0700

    cfg fix (sementara), added test case

--- test/test2.js
+++ test/test2.js
@@ -1,8 +1,16 @@
-const x ;   
+var x = 0;   
+var y = 1; 
 let N = 10 ;  
 
 while ( x  <=  10 ) {
-    console.log() ;
+    console.log( "LosPollosHermanos" ) ;
+
+    for ( let i = 0 ; i < 6 ; i ++ ) {
+        var temp = 1 ; 
+        y <<= 2 ;  
+    }
+    x ++ ; 
+
     if ( x === 5 ) {
         console.log() ;
     }
@@ -11,6 +19,7 @@ while ( x  <=  10 ) {
 {
     const nice = 69 ;
     const life = 42 ;
+    delete earth.existence ; 
 
     if ( nice === life ) {
         answer = null ;
